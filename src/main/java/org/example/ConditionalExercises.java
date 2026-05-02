fdb8a78867ab0ac56d9412a254d339c7a0f0a1e4
Siva Basani <sivabasani95@gmail.com>
2026-05-02 17:45:26 -0500

    Remove .idea from repo

--- src/main/java/org/example/ConditionalExercises.java
+++ src/main/java/org/example/ConditionalExercises.java
@@ -1,25 +1,62 @@
 package org.example;
 
-public class ConditionalExercises {
+
+public class ConditionalExercises{
     public boolean lessThanFive(int x) {
         // Replace the line below with code that returns true if x is less than 5, and false otherwise
-        // (use an if statement with an else block.)
-        return false;
+        // (use an if stat;
+        if (x < 5) {
+            return true;
+        }else {
+            return false;
+        }
+
+
     }
 
     public String getAgeGroup(int age) {
         // Replace the line below with code that returns "child" if age is less than 13,
         // "teen" if age is less than 20, and "adult" otherwise
         // (use if, else if, and else)
+
+
         return "";
     }
 
     public boolean isValidPassword(String password) {
         int passwordLength = password.length();
-
-        // Replace the line below with code that returns true if password is at least 8 characters long
+                // Replace the line below with code that returns true if password is at least 8 characters long
         // and false otherwise
         // (use an if statement with an else block.)
+        if (x < 5){
         return false;
-    }
+    }else {
+            return true;
+        }
+
+}
+
+public boolean lessThanFive(int x) {
+    // Replace the line below with code that returns true if x is less than 5, and false otherwise
+    // (use an if statement with an else block.)
+    return false;
+}
+
+public String getAgeGroup(int age) {
+    // Replace the line below with code that returns "child" if age is less than 13,
+    // "teen" if age is less than 20, and "adult" otherwise
+    // (use if, else if, and else)
+
+
+    return "";
+}
+
+public boolean isValidPassword(String password) {
+    int passwordLength = password.length();
+
+    // Replace the line below with code that returns true if password is at least 8 characters long
+    // and false otherwise
+    // (use an if statement with an else block.)
+    return false;
+}
 }
