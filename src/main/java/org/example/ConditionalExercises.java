eed0ddaeccf261ff158ff7beff08f7a7246c960b
sivabasani95 <sivabasani95@gmail.com>
2026-05-04 10:32:08 -0500

    "Fix ConditionalExercises: add missing methods, correct logic, and match test requirements"

--- src/main/java/org/example/ConditionalExercises.java
+++ src/main/java/org/example/ConditionalExercises.java
@@ -1,62 +1,38 @@
 package org.example;
 
+public class ConditionalExercises {
 
-public class ConditionalExercises{
-    public boolean lessThanFive(int x) {
-        // Replace the line below with code that returns true if x is less than 5, and false otherwise
-        // (use an if stat;
-        if (x < 5) {
-            return true;
-        }else {
-            return false;
-        }
-
-
-    }
-
-    public String getAgeGroup(int age) {
-        // Replace the line below with code that returns "child" if age is less than 13,
-        // "teen" if age is less than 20, and "adult" otherwise
-        // (use if, else if, and else)
-
-
-        return "";
-    }
-
-    public boolean isValidPassword(String password) {
-        int passwordLength = password.length();
-                // Replace the line below with code that returns true if password is at least 8 characters long
-        // and false otherwise
-        // (use an if statement with an else block.)
-        if (x < 5){
-        return false;
-    }else {
-            return true;
-        }
-
-}
-
-public boolean lessThanFive(int x) {
     // Replace the line below with code that returns true if x is less than 5, and false otherwise
     // (use an if statement with an else block.)
-    return false;
-}
+    public boolean lessThanFive(int x) {
+        if (x < 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-public String getAgeGroup(int age) {
     // Replace the line below with code that returns "child" if age is less than 13,
     // "teen" if age is less than 20, and "adult" otherwise
     // (use if, else if, and else)
-
-
-    return "";
-}
-
-public boolean isValidPassword(String password) {
-    int passwordLength = password.length();
+    public String getAgeGroup(int age) {
+        if (age < 13) {
+            return "child";
+        } else if (age < 20) {
+            return "teen";
+        } else {
+            return "adult";
+        }
+    }
 
     // Replace the line below with code that returns true if password is at least 8 characters long
     // and false otherwise
     // (use an if statement with an else block.)
-    return false;
-}
+    public boolean isValidPassword(String password) {
+        if (password.length() >= 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
